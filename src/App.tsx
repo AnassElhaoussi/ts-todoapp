47bfea7a95fd4638d38319f7d73b22fe2ba95682
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-12-05 22:35:52 +0100

    feat : adding the updating functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,51 @@ import './styles/index.css'
 import Todos from './Todos'
 
 export interface ITodos {
-  todo: string,
-  completed: boolean,
-  id: number
+  todo: string
+  completed: boolean
 }
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodos[]>([])
   const [todo, setTodo] = useState<string>("")
-  const id = useRef<number>(0)
+  const [isUpdated, setIsUpdated] = useState<boolean>(false)
+  const [id, setId] = useState<number | null>(null)
 
   const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
-    id.current++
-    setTodos((prev) => [...prev, {
+    if(todo.trim().length > 0) setTodos((prev) => [...prev, {
       todo,
       completed: false,
-      id: id.current
     }])
     setTodo("")
-    console.log(todos)
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value)
   }
 
+  const updateTodo = () => {
+    if(todo.trim().length > 0) {
+      const updated = todos.map((task, k) => {
+        if(id === k) return {
+          ...task,
+          todo: todo
+        }
+        else return task
+      })
+      setTodos(updated)
+      setIsUpdated(false)
+    } 
+    setTodo('')
+  }
+
+
   return (
     <div className='flex
      items-center justify-center bg-gray-100 min-h-screen font-body'>
       <div className='flex flex-col items-start gap-6'>
         <h1 className='text-6xl font-bold'>Hi there,</h1>
-        <form action="" onSubmit={onSubmit}>
+        <form className='flex flex-col items-start gap-2' action="" onSubmit={onSubmit}>
           <input 
           type="text" 
           placeholder='Add a todo'
@@ -45,9 +58,17 @@ const App: React.FC = () => {
           onChange={onChange}
           />
         </form>
+          <button 
+          style={{opacity: isUpdated ? '1' : '0.2'}}
+          className='p-2 bg-yellow-400 rounded-md'
+          onClick={updateTodo}
+          >Update</button>
         <Todos 
+        setTodo={setTodo}
         todos={todos}
         setTodos={setTodos}
+        setIsUpdated={setIsUpdated}
+        setId={setId}
         />
       </div>
     </div>
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 import {ITodos} from './App'
 
 interface ITodosProps {
-    todos: ITodos[],
+    todos: ITodos[]
+    setTodo: React.Dispatch<React.SetStateAction<string>>
     setTodos: React.Dispatch<React.SetStateAction<ITodos[]>>
+    setIsUpdated: React.Dispatch<React.SetStateAction<boolean>>
+    setId: React.Dispatch<React.SetStateAction<number | null>>
 }
 
-const Todos: React.FC<ITodosProps> = ({todos, setTodos}) => {
-    function handleDelete<T>(k: T){
-        const filteredTodos = todos.filter((todo) => {
-            return todo.id !== k
-        })
+const Todos: React.FC<ITodosProps> = ({
+    todos,
+    setTodos,
+    setTodo,
+    setIsUpdated, 
+    setId}) => {
 
+    function handleDelete<T>(k: T){
+        const filteredTodos = todos.filter((todo, id) => {
+            return id !== k
+        })
         setTodos(filteredTodos)
+    }
+
+    const handleUpdate = (currentTodo: string, id: number) => {
+        setTodo(currentTodo)
+        setIsUpdated(true)
+        setId(id)
     }
     return (
         <div className='flex flex-col gap-y-5 items-start'>
-            {todos.map(({todo, completed, id}) => {
+            {todos.map(({todo, completed}, id) => {
                 return (
                     <div className='flex p-3 rounded-md bg-gray-200 gap-5 items-center'>
                         <span 
                         >{todo}</span>
                         <div className='space-x-3'>
-                            <FontAwesomeIcon icon={faCheck} />
+                            <FontAwesomeIcon 
+                            icon={faPen}
+                            onClick={() => handleUpdate(todo, id)}
+                             />
                             <FontAwesomeIcon 
                             icon={faTrash}
                             onClick={() => handleDelete(id)}
